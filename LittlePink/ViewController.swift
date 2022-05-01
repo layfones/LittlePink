b15883c131ac04dd825b357ba0fb8e1140a515eb
lazyfront <lazyfront@gmail.com>
2022-05-01 18:27:54 +0800

    Initial Commit

---
+++ LittlePink/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittlePink
+//
+//  Created by Layfones on 2022/5/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
